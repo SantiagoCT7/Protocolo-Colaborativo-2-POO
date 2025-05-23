e149f12cb407716b045bf7f18c4c53226c485489
SantiagoCT7 <scardonat@unicartagena.edu.co>
2025-05-23 17:31:00 -0500

    Delete 7502420016 Santiago Cardoza Tapia Taller 6 Unidad 2 directory

--- 7502420016 Santiago Cardoza Tapia Taller 6 Unidad 2/ejercicio 6.2/PruebaVehiculo.java
+++
@@ -1,8 +0,0 @@
-//ejercicio 6.2
-public class PruebaVehiculo {
-    public static void main(String[] args) {
-        Moto m = new Moto("Motocicleta", "Kawasaki", 950);
-
-        m.mostrarInfo();
-    }
-}
